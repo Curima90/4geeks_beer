4a054bd9248c3d5e2e5b071f1813421422e9b598
Maurizio Curione <122744151+Curima90@users.noreply.github.com>
2024-07-11 21:49:11 +0000

    name filtering added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+
 const PORT = 25000;
-const beers = require("./beers");
+const beers = require("./beers.js");
 
 const app = express();
 // console.log(beers);
@@ -9,7 +10,30 @@ app.get("/", (request, response) => {
   response.send("this is my first route");
 });
 
+
+app.get("/beers", (request, response) => {
+  response.json(beers)
+})
+
+
+app.get("/beer/:id", (request, response) => {
+  let {id} = request.params
+  response.json(beers.filter(beer => {
+    return beer.id === parseInt(id)
+  }))
+})
+
+app.get("/beerss/:name", (request, response) => {
+  let {name} = request.params
+  response.json(beers.filter(beer => {
+    return beer.name === name
+  }))
+})
+
+
+app.post
+
+
 app.listen(PORT, () => {
   console.log(`user connected on port: ${PORT}`);
 });
-``;
